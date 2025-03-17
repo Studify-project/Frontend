124401fd16074ac62bddd5470525e9cbcc6488a7
lyn0719 <usolnim@naver.com>
2025-03-17 15:35:32 +0900

    init: svgr 설정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logo from '../assets/icons/Logo.svg?react';
 
 const MainPage: React.FC = () => {
   return (
     <div>
       <h1>Main Page</h1>
+      <Logo width={50} height={50} />
     </div>
   );
 };
